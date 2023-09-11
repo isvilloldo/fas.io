4ebb413af561f08e4ab10d7e7d1620bb736f86a5
ian <ian.villoldo@stmartin.edu>
2023-09-10 20:00:16 -0700

    initial commit

---
+++ Untitled-2.php
@@ -0,0 +1,293 @@
+<style>
+			.wrap {
+				  float: left; 
+				  margin: 5px;
+			 }
+	   </style>
+
+
+
+
+
+
+
+
+<h3 class="major">Instructors</h3>
+								<p><a href="file:///C:/Users/isvil/OneDrive/Desktop/FAS%20rebuild/FAS%20rebuild/dimension/instructors/index.html"><strong>Our Entire Team</strong></a></p>
+								<div class="wrap">
+   								<img src="images/bmr22.jpg" alt="">
+								   
+
+								</div>
+									<div class="text">
+									<p><strong><u>Belle McCormack</u></strong> is the Owner, Director, and Lead Instructor for The Firearms Academy of Seattle. She started teaching in 2016, and purchased FAS in 2022. Belle was formerly a Private Security Certified Handgun Instructor for the Washington State Criminal Justice Training Commission and taught in their Instructor Certification Program. She also holds an instructor certification from Tom Givens/Rangemaster, Massad Ayoob Group Deadly Force Instructor, Shot-IQ Pistol Instructor, and is a NRA Certified Pistol Instructor. She has trained extensively with Massad Ayoob, John Farnam, Chuck Taylor, Gunsite, Terry Trahan, Rory Miller, Marc Macyoung, Karl Rehn, Ed Monk, Steve Fisher, Jared Reston, Tim Herron, Kerry Davis, Mike Pannone, Bill Blowers, Robert Vogel, Scott Jedlinski, Alex Hartman, and Joel Turner of Shot IQ. Belle has over 20 years of experience in training, coaching, and other adult education. Before coming to FAS, she was the volunteer rangemaster at Jefferson County Sportsmans’ Association from 2014 to 2016 and assisted at Arms Pro, LCC, a gunsmith in Port Townsend, WA.</p>
+									</div>
+									<div class="wrap">
+										<img src="images/erik-knise-staff-2022-300px.jpg" alt="">
+										
+	 
+									 </div>
+										 <div class="text">
+										 <p><strong><u>Erik Knise</u></strong> is the Operations Manager, resident tech guy, and a Staff Instructor for FAS. He started training here in 2016 and after completing nearly the entire FAS curriculum in 2017 he was invited into the FAS Apprenticeship Program where he received his instructor certifications in Handgun, Rifle, and Shotgun in 2018. Erik is a Private Security Handgun Instructor for the Washington State Criminal Justice Training Commission and has instructor certifications from Tom Givens/Rangemaster, MAG Deadly Force Instructor, Patrol Rifle and RDS Instructor from Centrifuge Training, Shot-IQ Pistol Instructor, Patrol Shotgun Instructor from WA CJTC, and is a NRA Certified Pistol Instructor. His outside training includes Massad Ayoob, John Farnam, Chuck Taylor, Tom Givens, Gunsite, Rob Leatham, Larry Vickers, Jared Reston, Steve Fisher, Zach Campbell, Tim Herron, Mike Pannone, Earnest Langdon, John Holschen, Gabe White, Caleb Causey, William Aprill, Paul Sharp, Varg Freeborn, Lee Weems, John Hearne, Karl Rehn, Ed Monk, John Murphy, Marc MacYoung, Terry Trahan, Kerry Davis, Dan Smith/Centrifuge, Bones LSP/Centrifuge, Joel Turner Shot-IQ, and has taken multiple shoot house classes, including force on force, with Joe Weyer of Alliance Police Training and Steve Fisher of Sentinel Concepts. Erik is the only person to have passed Chuck Taylor’s Handgun Combat Masters Test with a traditional DA/SA (Beretta 92A1). He is a Master class IDPA shooter and won the Stock Service Pistol division at the 2018 Washington State IDPA Championship hosted here at FAS. He also holds Tim Herron's Barrel Drill Patch #28. Outside of FAS, Erik works full time in the industry at a firearms manufacturing company.</p>
+										 </div>
+										 <ul class="icons">
+											<li><a href="https://www.facebook.com/profile.php?id=100057586054499" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+											<li><a href="https://www.instagram.com/firearmsacademyofseattle" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+										</ul>
+								</article>
+
+
+
+<!-- Elements -->
+							<article id="elements">
+								<h2 class="major">Elements</h2>
+
+								<section>
+									<h3 class="major">Text</h3>
+									<p>This is <b>bold</b> and this is <strong>strong</strong>. This is <i>italic</i> and this is <em>emphasized</em>.
+									This is <sup>superscript</sup> text and this is <sub>subscript</sub> text.
+									This is <u>underlined</u> and this is code: <code>for (;;) { ... }</code>. Finally, <a href="#">this is a link</a>.</p>
+									<hr />
+									<h2>Heading Level 2</h2>
+									<h3>Heading Level 3</h3>
+									<h4>Heading Level 4</h4>
+									<h5>Heading Level 5</h5>
+									<h6>Heading Level 6</h6>
+									<hr />
+									<h4>Blockquote</h4>
+									<blockquote>Fringilla nisl. Donec accumsan interdum nisi, quis tincidunt felis sagittis eget tempus euismod. Vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan faucibus. Vestibulum ante ipsum primis in faucibus lorem ipsum dolor sit amet nullam adipiscing eu felis.</blockquote>
+									<h4>Preformatted</h4>
+									<pre><code>i = 0;
+
+while (!deck.isInOrder()) {
+    print 'Iteration ' + i;
+    deck.shuffle();
+    i++;
+}
+
+print 'It took ' + i + ' iterations to sort the deck.';</code></pre>
+								</section>
+
+								<section>
+									<h3 class="major">Lists</h3>
+
+									<h4>Unordered</h4>
+									<ul>
+										<li>Dolor pulvinar etiam.</li>
+										<li>Sagittis adipiscing.</li>
+										<li>Felis enim feugiat.</li>
+									</ul>
+
+									<h4>Alternate</h4>
+									<ul class="alt">
+										<li>Dolor pulvinar etiam.</li>
+										<li>Sagittis adipiscing.</li>
+										<li>Felis enim feugiat.</li>
+									</ul>
+
+									<h4>Ordered</h4>
+									<ol>
+										<li>Dolor pulvinar etiam.</li>
+										<li>Etiam vel felis viverra.</li>
+										<li>Felis enim feugiat.</li>
+										<li>Dolor pulvinar etiam.</li>
+										<li>Etiam vel felis lorem.</li>
+										<li>Felis enim et feugiat.</li>
+									</ol>
+									<h4>Icons</h4>
+									<ul class="icons">
+										<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+										<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+										<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+										<li><a href="#" class="icon brands fa-github"><span class="label">Github</span></a></li>
+									</ul>
+
+									<h4>Actions</h4>
+									<ul class="actions">
+										<li><a href="#" class="button primary">Default</a></li>
+										<li><a href="#" class="button">Default</a></li>
+									</ul>
+									<ul class="actions stacked">
+										<li><a href="#" class="button primary">Default</a></li>
+										<li><a href="#" class="button">Default</a></li>
+									</ul>
+								</section>
+
+								<section>
+									<h3 class="major">Table</h3>
+									<h4>Default</h4>
+									<div class="table-wrapper">
+										<table>
+											<thead>
+												<tr>
+													<th>Name</th>
+													<th>Description</th>
+													<th>Price</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>Item One</td>
+													<td>Ante turpis integer aliquet porttitor.</td>
+													<td>29.99</td>
+												</tr>
+												<tr>
+													<td>Item Two</td>
+													<td>Vis ac commodo adipiscing arcu aliquet.</td>
+													<td>19.99</td>
+												</tr>
+												<tr>
+													<td>Item Three</td>
+													<td> Morbi faucibus arcu accumsan lorem.</td>
+													<td>29.99</td>
+												</tr>
+												<tr>
+													<td>Item Four</td>
+													<td>Vitae integer tempus condimentum.</td>
+													<td>19.99</td>
+												</tr>
+												<tr>
+													<td>Item Five</td>
+													<td>Ante turpis integer aliquet porttitor.</td>
+													<td>29.99</td>
+												</tr>
+											</tbody>
+											<tfoot>
+												<tr>
+													<td colspan="2"></td>
+													<td>100.00</td>
+												</tr>
+											</tfoot>
+										</table>
+									</div>
+
+									<h4>Alternate</h4>
+									<div class="table-wrapper">
+										<table class="alt">
+											<thead>
+												<tr>
+													<th>Name</th>
+													<th>Description</th>
+													<th>Price</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>Item One</td>
+													<td>Ante turpis integer aliquet porttitor.</td>
+													<td>29.99</td>
+												</tr>
+												<tr>
+													<td>Item Two</td>
+													<td>Vis ac commodo adipiscing arcu aliquet.</td>
+													<td>19.99</td>
+												</tr>
+												<tr>
+													<td>Item Three</td>
+													<td> Morbi faucibus arcu accumsan lorem.</td>
+													<td>29.99</td>
+												</tr>
+												<tr>
+													<td>Item Four</td>
+													<td>Vitae integer tempus condimentum.</td>
+													<td>19.99</td>
+												</tr>
+												<tr>
+													<td>Item Five</td>
+													<td>Ante turpis integer aliquet porttitor.</td>
+													<td>29.99</td>
+												</tr>
+											</tbody>
+											<tfoot>
+												<tr>
+													<td colspan="2"></td>
+													<td>100.00</td>
+												</tr>
+											</tfoot>
+										</table>
+									</div>
+								</section>
+
+								<section>
+									<h3 class="major">Buttons</h3>
+									<ul class="actions">
+										<li><a href="#" class="button primary">Primary</a></li>
+										<li><a href="#" class="button">Default</a></li>
+									</ul>
+									<ul class="actions">
+										<li><a href="#" class="button">Default</a></li>
+										<li><a href="#" class="button small">Small</a></li>
+									</ul>
+									<ul class="actions">
+										<li><a href="#" class="button primary icon solid fa-download">Icon</a></li>
+										<li><a href="#" class="button icon solid fa-download">Icon</a></li>
+									</ul>
+									<ul class="actions">
+										<li><span class="button primary disabled">Disabled</span></li>
+										<li><span class="button disabled">Disabled</span></li>
+									</ul>
+								</section>
+
+								<section>
+									<h3 class="major">Form</h3>
+									<form method="post" action="#">
+										<div class="fields">
+											<div class="field half">
+												<label for="demo-name">Name</label>
+												<input type="text" name="demo-name" id="demo-name" value="" placeholder="Jane Doe" />
+											</div>
+											<div class="field half">
+												<label for="demo-email">Email</label>
+												<input type="email" name="demo-email" id="demo-email" value="" placeholder="jane@untitled.tld" />
+											</div>
+											<div class="field">
+												<label for="demo-category">Category</label>
+												<select name="demo-category" id="demo-category">
+													<option value="">-</option>
+													<option value="1">Manufacturing</option>
+													<option value="1">Shipping</option>
+													<option value="1">Administration</option>
+													<option value="1">Human Resources</option>
+												</select>
+											</div>
+											<div class="field half">
+												<input type="radio" id="demo-priority-low" name="demo-priority" checked>
+												<label for="demo-priority-low">Low</label>
+											</div>
+											<div class="field half">
+												<input type="radio" id="demo-priority-high" name="demo-priority">
+												<label for="demo-priority-high">High</label>
+											</div>
+											<div class="field half">
+												<input type="checkbox" id="demo-copy" name="demo-copy">
+												<label for="demo-copy">Email me a copy</label>
+											</div>
+											<div class="field half">
+												<input type="checkbox" id="demo-human" name="demo-human" checked>
+												<label for="demo-human">Not a robot</label>
+											</div>
+											<div class="field">
+												<label for="demo-message">Message</label>
+												<textarea name="demo-message" id="demo-message" placeholder="Enter your message" rows="6"></textarea>
+											</div>
+										</div>
+										<ul class="actions">
+											<li><input type="submit" value="Send Message" class="primary" /></li>
+											<li><input type="reset" value="Reset" /></li>
+										</ul>
+									</form>
+								</section>
+
+							</article>
+
+					</div>
+
+
+					
+
+						<!-- Contact -->
+						<article id="contact">
+								<h3 class="major">The Firearms Academy of Seattle</h3>
+								<p>3399 Centralia Alpha Rd, Onalaska, WA 98570</p>
+								<p>360-978-6100</p>
+								<p>Please contact us before arranging any deliveries.</p>
